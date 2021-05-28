328e465cd1c31ea0c06757d409651084e98e8a1a
Clarence Siah <clarence.jun.da.siah@continental-corporation.com>
2021-05-28 21:31:21 +0800

    Initial revision with findAll from dynamo

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+// snippet-start:[dynamodb.go.load_items.imports]
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Movie struct {
+	ID   string
+	Name string
+}
+
+func getItems() []Movie {
+	raw, err := ioutil.ReadFile("./movies.json")
+	if err != nil {
+		log.Fatalf("Got error reading file: %s", err)
+	}
+
+	var items []Movie
+	json.Unmarshal(raw, &items)
+	return items
+}
+
+func findAll(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	size, err := strconv.Atoi(request.Headers["Count"])
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       "Count Header should be a number",
+		}, nil
+	}
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	req, res := svc.ScanRequest(&dynamodb.ScanInput{
+		TableName: aws.String("movies"),
+		Limit:     aws.Int64(int64(size)),
+	})
+
+	err = req.Send()
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusTeapot,
+			Body:       "Count Header should be a number",
+		}, nil
+	}
+
+	response, err := json.Marshal(res.Items)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(response),
+	}, nil
+}
+
+func main() {
+	lambda.Start(findAll)
+}
